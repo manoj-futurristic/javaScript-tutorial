1cb292aeeba3df639bff3785e620275e1ad5df61
acareindore <manojjaiswal@futurristic.com>
2023-02-14 11:17:20 +0530

    exercises

--- Math/math.js
+++ Math/math.js
@@ -29,21 +29,32 @@ print(`sqrt => ${Math.sqrt(64)}`);
 // Math.abs(x) returns the absolute (positive) value of x:
 print(`abs => ${Math.abs(-4.5)}`);
 
-// Math.min() and Math.max() can be used to find the lowest or highest value in a list of arguments:Math.min() and Math.max() can be used to find the lowest or highest value in a list of arguments:
+// Math.min() and Math.max() can be used to find the lowest or highest value in a list of arguments:
+//Math.min() and Math.max() can be used to find the lowest or highest value in a list of arguments:
 
 print(`MAX => ${Math.max(1,2,3,4,5,5,56,6)}`);
 print(`MIN => ${Math.min(1,2,3,4,5,5,56,6)}`);
 
 
-
-
-
-
-
 function random(min, max) {
-    const num = Math.floor(Math.random() * (max - min + 1)) + min;
-    return num;
-  };
-  
+  const num = Math.floor(Math.random() * (max - min + 1)) + min;
+  return num;
+};
+
 print(random(1, 10000));
 
+
+
+const fav = (start, end, length) =>{
+  let i,sum;
+  const ser = [];
+  for(i=0;i<length;i++){
+    ser.push(start);
+    sum = start + end;
+    start=end;
+    end = sum;
+  }
+  print(ser);
+};
+
+fav(0,1,10);
